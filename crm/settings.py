b20ef55007b9900e9541800683718d1546b7c62e
mjablonski984 <mjablonski984@gmail.com>
2021-03-26 18:58:22 +0000

    Configure database conection

--- crm/settings.py
+++ crm/settings.py
@@ -90,6 +90,11 @@ DATABASES = {
     }
 }
 
+# use dj_database_url to configure DATABASE_URL environment variable for Heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
